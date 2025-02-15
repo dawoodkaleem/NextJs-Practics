d120470bb1184836a79f9c60ff7a7a42621bd58e
dawoodkaleem <dawood9743@gmail.com>
2025-02-15 21:24:40 +0500

    updated

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -1,6 +1,6 @@
 export default async function ProductDetails({params,}:{params:Promise<{productId:string}>}){ const productId =(await params).productId
   return(<>
 
-  <h1>Details about products {productId} </h1>
+  <h1>Details about products {productId} {productId}  {productId} {productId} </h1>
   </>)
 }
