372b50242b68b529d88d01c06167d82770201716
dawoodkaleem <dawood9743@gmail.com>
2025-03-05 22:23:41 +0500

    created

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,11 +1,21 @@
+import Link from "next/link";
 const ProductsList = () => {
   return (
     <>
+      <Link href="/">Home</Link>
       <h1>Product list</h1>
-      <h2>Product 1</h2>
-      <h2>Product 2</h2>
-      <h2>Product 3</h2>
-      <h2>Product 4</h2>
+      <h2>
+        <Link href="/product/1">Product 1</Link>
+      </h2>
+      <h2>
+        <Link href="/product/2">Product 2</Link>
+      </h2>
+      <h2>
+        <Link href="/product/3" replace>Product 3</Link>
+      </h2>
+      <h2>
+        <Link href="/product/4">Product 4</Link>
+      </h2>
     </>
   );
 };
